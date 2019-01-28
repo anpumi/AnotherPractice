cd4c341b32ac13ebdf44aea6d4e3c0278ce5cab0
anpumi <annukka.puotiniemi@gmail.com>
2019-01-28 10:07:25 +0200

    Lisätty hello world tulostus

--- GitHarjoitus/Program.cs
+++ GitHarjoitus/Program.cs
@@ -7,6 +7,7 @@ namespace GitHarjoitus
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hei maailma");
         }
     }
 }
